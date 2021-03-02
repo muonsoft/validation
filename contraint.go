874536ba66ac9e1d1b6edf05bc2d7aff6457cae7
Igor Lazarev <strider2038@yandex.ru>
2021-03-02 21:00:15 +0300

    constraint abstractions improved

--- contraint.go
+++ contraint.go
@@ -14,29 +14,36 @@ type Constraint interface {
 // NilConstraint is used for constraints that needs to check value for nil. In common case
 // you have no need to implement it in your constraints because nil values should be ignored.
 type NilConstraint interface {
+	Constraint
 	ValidateNil(scope Scope) error
 }
 
 type BoolConstraint interface {
+	Constraint
 	ValidateBool(value *bool, scope Scope) error
 }
 
 type NumberConstraint interface {
+	Constraint
 	ValidateNumber(value generic.Number, scope Scope) error
 }
 
 type StringConstraint interface {
+	Constraint
 	ValidateString(value *string, scope Scope) error
 }
 
 type IterableConstraint interface {
+	Constraint
 	ValidateIterable(value generic.Iterable, scope Scope) error
 }
 
 type CountableConstraint interface {
+	Constraint
 	ValidateCountable(count int, scope Scope) error
 }
 
 type TimeConstraint interface {
+	Constraint
 	ValidateTime(value *time.Time, scope Scope) error
 }
