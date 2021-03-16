b061c24a1fb2753796f7a82233626e608af767ab
Igor Lazarev <strider2038@yandex.ru>
2021-03-16 22:07:13 +0300

    constraint-comments

--- it/iterable.go
+++ it/iterable.go
@@ -34,37 +34,79 @@ func newCountConstraint(min int, max int, checkMin bool, checkMax bool) CountCon
 	}
 }
 
+// HasMinCount creates a CountConstraint that checks the length of the iterable (slice, array, or map)
+// is greater than the minimum value.
+//
+// Example
+//  v := []int{1, 2}
+//  err := validator.ValidateIterable(v, it.HasMinCount(3))
 func HasMinCount(min int) CountConstraint {
 	return newCountConstraint(min, 0, true, false)
 }
 
+// HasMaxCount creates a CountConstraint that checks the length of the iterable (slice, array, or map)
+// is less than the maximum value.
+//
+// Example
+//  v := []int{1, 2}
+//  err := validator.ValidateIterable(v, it.HasMaxCount(1))
 func HasMaxCount(max int) CountConstraint {
 	return newCountConstraint(0, max, false, true)
 }
 
+// HasCountBetween creates a CountConstraint that checks the length of the iterable (slice, array, or map)
+// is between some minimum and maximum value.
+//
+// Example
+//  v := []int{1, 2}
+//  err := validator.ValidateIterable(v, it.HasCountBetween(3, 10))
 func HasCountBetween(min int, max int) CountConstraint {
 	return newCountConstraint(min, max, true, true)
 }
 
+// HasExactCount creates a CountConstraint that checks the length of the iterable (slice, array, or map)
+// has exact value.
+//
+// Example
+//  v := []int{1, 2}
+//  err := validator.ValidateIterable(v, it.HasExactCount(3))
 func HasExactCount(count int) CountConstraint {
 	return newCountConstraint(count, count, true, true)
 }
 
+// Name is the constraint name.
+func (c CountConstraint) Name() string {
+	return "CountConstraint"
+}
+
+// SetUp always returns no error.
+func (c CountConstraint) SetUp() error {
+	return nil
+}
+
+// When enables conditional validation of this constraint. If the expression evaluates to false,
+// then the constraint will be ignored.
 func (c CountConstraint) When(condition bool) CountConstraint {
 	c.isIgnored = !condition
 	return c
 }
 
+// MinMessage sets the violation message that will be shown if the collection length is less than
+// the minimum value.
 func (c CountConstraint) MinMessage(message string) CountConstraint {
 	c.minMessageTemplate = message
 	return c
 }
 
+// MaxMessage sets the violation message that will be shown if the collection length is greater than
+// the maximum value.
 func (c CountConstraint) MaxMessage(message string) CountConstraint {
 	c.maxMessageTemplate = message
 	return c
 }
 
+// ExactMessage sets the violation message that will be shown if minimum and maximum values are equal and
+// the length of the collection is not exactly this value.
 func (c CountConstraint) ExactMessage(message string) CountConstraint {
 	c.exactMessageTemplate = message
 	return c
@@ -86,14 +128,6 @@ func (c CountConstraint) ValidateCountable(count int, scope validation.Scope) er
 	}
 
 	return nil
-}
-
-func (c CountConstraint) SetUp() error {
-	return nil
-}
-
-func (c CountConstraint) Name() string {
-	return "CountConstraint"
 }
 
 func (c CountConstraint) newViolation(
