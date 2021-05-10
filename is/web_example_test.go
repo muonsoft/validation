cc6af12c25c1fcf5c568e733bb9fb20b54ddf4e5
Igor Lazarev <strider2038@yandex.ru>
2021-05-10 18:07:13 +0300

    url-constraint * short examples

--- is/web_example_test.go
+++ is/web_example_test.go
@@ -6,34 +6,14 @@ import (
 	"github.com/muonsoft/validation/is"
 )
 
-func ExampleURL_validAbsoluteURL() {
-	valid := is.URL("https://example.com")
-
-	fmt.Println(valid)
+func ExampleURL() {
+	fmt.Println(is.URL("https://example.com"))                       // valid absolute URL
+	fmt.Println(is.URL("ftp://example.com", "http", "https", "ftp")) // valid URL with custom schema
+	fmt.Println(is.URL("example.com"))                               // invalid URL
+	fmt.Println(is.URL("//example.com", ""))                         // valid relative URL
 	// Output:
 	// true
-}
-
-func ExampleURL_validURLWithCustomSchema() {
-	valid := is.URL("ftp://example.com", "http", "https", "ftp")
-
-	fmt.Println(valid)
-	// Output:
 	// true
-}
-
-func ExampleURL_invalidURL() {
-	valid := is.URL("example.com")
-
-	fmt.Println(valid)
-	// Output:
 	// false
-}
-
-func ExampleURL_validRelativeURL() {
-	valid := is.URL("//example.com", "")
-
-	fmt.Println(valid)
-	// Output:
 	// true
 }
