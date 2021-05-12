37ec31bdccff80ab7e4664fdd312259ef7a9bdd4
rdavletshin <rdavletshin@goodwix.team>
2021-05-12 15:19:06 +0300

    hotfix_recursive-property-path
    * bug with collect recursive property path fixed

--- test/path_test.go
+++ test/path_test.go
@@ -3,11 +3,71 @@ package test
 import (
 	"testing"
 
+	"github.com/muonsoft/validation"
 	"github.com/muonsoft/validation/code"
 	"github.com/muonsoft/validation/it"
 	"github.com/muonsoft/validation/message"
 	"github.com/muonsoft/validation/validator"
 )
+
+type property struct {
+	Name  string
+	Type  string
+	Value []*property
+}
+
+func (p property) Validate(validator *validation.Validator) error {
+	arguments := []validation.Argument{
+		validation.StringProperty(
+			"name",
+			&p.Name,
+			it.IsNotBlank(),
+		),
+		validation.StringProperty(
+			"type",
+			&p.Type,
+			it.IsNotBlank(),
+		),
+	}
+
+	if p.Value != nil {
+		arguments = append(arguments,
+			validation.IterableProperty(
+				"value",
+				p.Value,
+				it.HasMinCount(1),
+			),
+		)
+	}
+
+	return validator.Validate(arguments...)
+}
+
+func TestValidate_AtProperty_WhenGivenRecursiveProperties_ExpectViolationWithProperty(t *testing.T) {
+	properties := []*property{
+		{
+			Name: "first level",
+			Type: "first type",
+			Value: []*property{
+				{
+					Name: "second level",
+					Type: "second type",
+					Value: []*property{
+						{
+							Name:  "",
+							Type:  "blank",
+							Value: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := validator.ValidateIterable(properties)
+
+	assertHasOneViolation(code.NotBlank, message.NotBlank, "[0].value[0].value[0].name")(t, err)
+}
 
 func TestValidate_AtProperty_WhenGivenProperty_ExpectViolationWithProperty(t *testing.T) {
 	err := validator.AtProperty("property").ValidateString(stringValue(""), it.IsNotBlank())
