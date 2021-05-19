301101beeffd2339f72eb15ae00c77db7f88ffdd
Igor Lazarev <strider2038@yandex.ru>
2021-05-19 18:02:23 +0300

    scope-validator

--- scope.go
+++ scope.go
@@ -40,6 +40,13 @@ func (s Scope) BuildViolation(code, message string) *ViolationBuilder {
 	return b
 }
 
+// Validator creates a new validation for the given scope. This validator can be used
+// to perform complex validation on a custom constraint by using the
+// already existing constraints.
+func (s Scope) Validator() *Validator {
+	return newScopedValidator(s)
+}
+
 func (s *Scope) applyOptions(options ...Option) error {
 	for _, option := range options {
 		var err error
