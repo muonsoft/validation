f899e6fd9697ca480f183b67ec6d7c7914c49d34
Igor Lazarev <strider2038@yandex.ru>
2022-07-16 22:56:59 +0300

    feature: unmarshal property path

--- path.go
+++ path.go
@@ -1,7 +1,9 @@
 package validation
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"unicode"
@@ -87,29 +89,58 @@ func (path *PropertyPath) WithIndex(index int) *PropertyPath {
 	}
 }
 
+// Elements returns property path as a slice of PropertyPathElement.
+// It returns nil if property path is nil (empty).
+func (path *PropertyPath) Elements() []PropertyPathElement {
+	if path == nil || path.value == nil {
+		return nil
+	}
+
+	length := path.Len()
+	elements := make([]PropertyPathElement, length)
+
+	i := length - 1
+	element := path
+	for element != nil {
+		elements[i] = element.value
+		element = element.parent
+		i--
+	}
+
+	return elements
+}
+
+// Len returns count of property path elements.
+func (path *PropertyPath) Len() int {
+	length := 0
+	element := path
+	for element != nil {
+		length++
+		element = element.parent
+	}
+	return length
+}
+
 // String is used to format property path to a string.
 func (path *PropertyPath) String() string {
-	elements := make([]PropertyPathElement, 0, 8)
-	element := path
+	elements := path.Elements()
 	count := 0
-	for element != nil {
-		if s, ok := element.value.(PropertyName); ok {
+	for _, element := range elements {
+		if s, ok := element.(PropertyName); ok {
 			count += len(s)
 		} else {
 			count += 2
 		}
-		elements = append(elements, element.value)
-		element = element.parent
 	}
 
 	s := strings.Builder{}
 	s.Grow(count)
-	for i := len(elements) - 1; i >= 0; i-- {
-		name := elements[i].String()
-		if elements[i].IsIndex() {
+	for i, element := range elements {
+		name := element.String()
+		if element.IsIndex() {
 			s.WriteString("[" + name + "]")
 		} else if isIdentifier(name) {
-			if i < len(elements)-1 {
+			if i > 0 {
 				s.WriteString(".")
 			}
 			s.WriteString(name)
@@ -128,20 +159,41 @@ func (path *PropertyPath) MarshalText() (text []byte, err error) {
 	return []byte(path.String()), nil
 }
 
+// UnmarshalText unmarshal string representation of property path into PropertyPath.
+func (path *PropertyPath) UnmarshalText(text []byte) error {
+	parser := pathParser{}
+	p, err := parser.Parse(string(text))
+	if p == nil || err != nil {
+		return err
+	}
+
+	*path = *p
+
+	return nil
+}
+
 func isIdentifier(s string) bool {
 	if len(s) == 0 {
 		return false
 	}
 	for i, c := range s {
-		if i == 0 && !unicode.IsLetter(c) {
+		if i == 0 && !isFirstIdentifierChar(c) {
 			return false
 		}
-		if i > 0 && !unicode.IsLetter(c) && !unicode.IsDigit(c) {
+		if i > 0 && !isIdentifierChar(c) {
 			return false
 		}
 	}
 
 	return true
+}
+
+func isFirstIdentifierChar(c rune) bool {
+	return unicode.IsLetter(c) || c == '$' || c == '_'
+}
+
+func isIdentifierChar(c rune) bool {
+	return unicode.IsLetter(c) || unicode.IsDigit(c) || c == '$' || c == '_'
 }
 
 func writePropertyName(s *strings.Builder, name string) {
@@ -151,4 +203,294 @@ func writePropertyName(s *strings.Builder, name string) {
 		}
 		s.WriteRune(c)
 	}
+}
+
+type parsingState byte
+
+const (
+	initialState parsingState = iota
+	beginIdentifierState
+	identifierState
+
+	beginIndexState
+	indexState
+
+	bracketedNameState
+	endBracketedNameState
+
+	closeBracketState
+)
+
+type pathParser struct {
+	buffer    strings.Builder
+	state     parsingState
+	isEscape  bool
+	index     int
+	pathIndex int
+	path      *PropertyPath
+}
+
+func (parser *pathParser) Parse(encodedPath string) (*PropertyPath, error) {
+	if len(encodedPath) == 0 {
+		return nil, nil
+	}
+	for i, c := range encodedPath {
+		parser.index = i
+		if err := parser.handleNext(c); err != nil {
+			return nil, err
+		}
+	}
+
+	return parser.finish()
+}
+
+func (parser *pathParser) handleNext(c rune) error {
+	var err error
+
+	switch c {
+	case '[':
+		err = parser.handleOpenBracket(c)
+	case ']':
+		err = parser.handleCloseBracket(c)
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		err = parser.handleDigit(c)
+	case '\'':
+		err = parser.handleQuote(c)
+	case '\\':
+		err = parser.handleEscape(c)
+	case '.':
+		err = parser.handlePoint(c)
+	default:
+		err = parser.handleOther(c)
+	}
+
+	return err
+}
+
+func (parser *pathParser) handleOpenBracket(c rune) error {
+	switch parser.state {
+	case beginIdentifierState, beginIndexState, indexState, endBracketedNameState:
+		return parser.newCharError(c, "unexpected char")
+	case identifierState:
+		if parser.buffer.Len() > 0 {
+			parser.addProperty()
+		}
+		parser.state = beginIndexState
+	case initialState, closeBracketState:
+		parser.state = beginIndexState
+	case bracketedNameState:
+		parser.buffer.WriteRune(c)
+	}
+
+	return nil
+}
+
+func (parser *pathParser) handleCloseBracket(c rune) error {
+	switch parser.state {
+	case indexState:
+		err := parser.addIndex()
+		if err != nil {
+			return err
+		}
+		parser.state = closeBracketState
+	case bracketedNameState:
+		parser.buffer.WriteRune(c)
+	case endBracketedNameState:
+		parser.addProperty()
+		parser.state = closeBracketState
+	default:
+		return parser.newCharError(c, "unexpected close bracket")
+	}
+
+	return nil
+}
+
+func (parser *pathParser) handleDigit(c rune) error {
+	switch parser.state {
+	case beginIndexState, indexState:
+		parser.state = indexState
+	case bracketedNameState, identifierState:
+	case initialState, beginIdentifierState:
+		return parser.newCharError(c, "unexpected identifier character")
+	default:
+		return parser.newCharError(c, "invalid array index")
+	}
+	parser.buffer.WriteRune(c)
+
+	return nil
+}
+
+func (parser *pathParser) handlePoint(c rune) error {
+	switch parser.state {
+	case beginIdentifierState, identifierState:
+		if parser.buffer.Len() == 0 {
+			return parser.newCharError(c, "unexpected point")
+		}
+		parser.addProperty()
+		parser.state = beginIdentifierState
+	case bracketedNameState:
+		parser.buffer.WriteRune(c)
+	case closeBracketState:
+		parser.state = beginIdentifierState
+	default:
+		return parser.newCharError(c, "unexpected point")
+	}
+
+	return nil
+}
+
+func (parser *pathParser) handleQuote(c rune) error {
+	if parser.isEscape {
+		parser.buffer.WriteRune(c)
+		parser.isEscape = false
+
+		return nil
+	}
+
+	switch parser.state {
+	case beginIndexState:
+		parser.state = bracketedNameState
+	case bracketedNameState:
+		parser.state = endBracketedNameState
+	default:
+		return parser.newCharError(c, "unexpected quote")
+	}
+
+	return nil
+}
+
+func (parser *pathParser) handleEscape(c rune) error {
+	if parser.state != bracketedNameState {
+		return parser.newCharError(c, "unexpected backslash")
+	}
+
+	if parser.isEscape {
+		parser.buffer.WriteRune(c)
+		parser.isEscape = false
+	} else {
+		parser.isEscape = true
+	}
+
+	return nil
+}
+
+func (parser *pathParser) handleOther(c rune) error {
+	switch parser.state {
+	case beginIndexState, indexState:
+		return parser.newCharError(c, "unexpected array index character")
+	case initialState, beginIdentifierState, identifierState:
+		if !isFirstIdentifierChar(c) {
+			return parser.newCharError(c, "unexpected identifier char")
+		}
+		parser.state = identifierState
+	case closeBracketState, endBracketedNameState:
+		return parser.newCharError(c, "unexpected char")
+	}
+	parser.buffer.WriteRune(c)
+
+	return nil
+}
+
+func (parser *pathParser) addProperty() {
+	parser.path = parser.path.WithProperty(parser.buffer.String())
+	parser.pathIndex++
+	parser.buffer.Reset()
+}
+
+func (parser *pathParser) addIndex() error {
+	s := parser.buffer.String()
+
+	u, err := strconv.ParseUint(s, 10, 0)
+	if err != nil {
+		if errors.Is(err, strconv.ErrRange) {
+			return parser.newProcessingError("value out of range: " + s)
+		}
+		return parser.newProcessingError("invalid array index: " + s)
+	}
+	if u > math.MaxInt {
+		return parser.newProcessingError("value out of range: " + s)
+	}
+
+	parser.path = parser.path.WithIndex(int(u))
+	parser.pathIndex++
+	parser.buffer.Reset()
+
+	return nil
+}
+
+func (parser *pathParser) finish() (*PropertyPath, error) {
+	switch parser.state {
+	case beginIdentifierState, identifierState:
+		if parser.buffer.Len() == 0 {
+			return nil, parser.newError("incomplete property name")
+		}
+		parser.path = parser.path.WithProperty(parser.buffer.String())
+	case beginIndexState, indexState:
+		return nil, parser.newError("incomplete array index")
+	case bracketedNameState, endBracketedNameState:
+		return nil, parser.newError("incomplete bracketed property name")
+	case closeBracketState:
+	default:
+		return nil, parser.newError("unexpected parsing state")
+	}
+
+	return parser.path, nil
+}
+
+func (parser *pathParser) newError(message string) *pathParsingError {
+	return &pathParsingError{
+		pathIndex: parser.pathIndex,
+		message:   message,
+	}
+}
+
+func (parser *pathParser) newCharError(char rune, message string) *pathParsingCharError {
+	return &pathParsingCharError{
+		index:     parser.index,
+		pathIndex: parser.pathIndex,
+		char:      char,
+		message:   message,
+	}
+}
+
+func (parser *pathParser) newProcessingError(message string) *pathParsingProcessingError {
+	return &pathParsingProcessingError{
+		pathIndex: parser.pathIndex,
+		message:   message,
+	}
+}
+
+type pathParsingError struct {
+	pathIndex int
+	message   string
+}
+
+func (err *pathParsingError) Error() string {
+	return fmt.Sprintf("parsing path element #%d: %s", err.pathIndex, err.message)
+}
+
+type pathParsingCharError struct {
+	index     int
+	pathIndex int
+	char      rune
+	message   string
+}
+
+func (err *pathParsingCharError) Error() string {
+	return fmt.Sprintf(
+		"parsing path element #%d at char #%d %q: %s",
+		err.pathIndex,
+		err.index,
+		err.char,
+		err.message,
+	)
+}
+
+type pathParsingProcessingError struct {
+	pathIndex int
+	message   string
+}
+
+func (err *pathParsingProcessingError) Error() string {
+	return fmt.Sprintf("parsing path element #%d: %s", err.pathIndex, err.message)
 }
